cb63dced1c46f6bf07a81c6df04d1e896684beb4
OVIS <ovi@ovswebsites.com>
2023-11-03 14:48:48 +0200

    added env variables for Sanity Studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID || "";
+const dataset = process.env.NEXT_PUBLIC_SANITY_STUDIO_DATASET || "production";
+const title = process.env.NEXT_PUBLIC_SANITY_STUDIO_TITLE || "Env Title";
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_STUDIO_API_VERSION || "";
+const basePath = process.env.NEXT_PUBLIC_SANITY_STUDIO_BASE_PATH || "/admin";
+
 const config = defineConfig({
-  projectId: "anmky5yg",
-  dataset: "production",
-  title: "Praxis Ladar Website",
-  apiVersion: "2023-11-03",
-  basePath: "/admin",
+  projectId,
+  dataset,
+  title,
+  apiVersion,
+  basePath,
   plugins: [deskTool()],
 });
 
