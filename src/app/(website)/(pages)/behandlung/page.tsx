8291d7aeb44fc29a1d8e6508cfa57afeeffa13a5
Ovi Savescu <ovi@ovswebsites.com>
2023-11-27 14:55:24 +0200

    fixed mobile services not showing on behandlung page

--- src/app/(website)/(pages)/behandlung/page.tsx
+++ src/app/(website)/(pages)/behandlung/page.tsx
@@ -1,10 +1,19 @@
-import { SectionBehandlung } from "@/devlink";
+import { SectionBehandlung, BehandlungItemMobile } from "@/devlink";
 import React from "react";
 
 export default function ServicesPage() {
+  const servicesListMarkup = servicesArray.map((service, key) => {
+    return (
+      <BehandlungItemMobile
+        key={key}
+        link={{ href: `/behandlung/${service.Slug}` }}
+        text={service.Name}
+      />
+    );
+  });
   return (
     <>
-      <SectionBehandlung />
+      <SectionBehandlung listSlot={servicesListMarkup} />
     </>
   );
 }
