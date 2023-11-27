9d5dc3078a70cb50ea384758abb01d42e069ae1f
Ovi Savescu <ovi@ovswebsites.com>
2023-11-27 14:56:28 +0200

    made service page static

--- src/app/(website)/(pages)/behandlung/[service]/page.tsx
+++ src/app/(website)/(pages)/behandlung/[service]/page.tsx
@@ -2,18 +2,32 @@ import { notFound } from "next/navigation";
 import { getServicePage } from "@/app/(website)/data/utils";
 import { SectionLongContent } from "@/devlink";
 import ButtonRuntimeProps from "@/app/(website)/components/ButtonRuntimeProps";
+import servicesData from "@/app/(website)/data/services";
 
 type Props = {
   params: { service: string };
 };
 
-export default function ServicePage({ params }: Props) {
-  const slug = params.service;
-  const page = getServicePage(slug);
+export async function generateStaticParams() {
+  //  we're looking for something like: [{ service: "asthetische-zahnheilkunde" }, { service: "bleaching" }, ... etc...];
+  return servicesData.map((page) => {
+    return {
+      service: page.Slug,
+    };
+  });
+}
 
-  if (page === undefined) {
-    notFound();
-  }
+function getService(params: { service: string }) {
+  const service = getServicePage(params.service);
+  return service;
+}
+
+export default function ServicePage({ params }: Props) {
+  const page = getService(params);
+
+  // if (page === undefined) {
+  //   notFound();
+  // }
 
   return (
     <main>
