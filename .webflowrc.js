6a85bc0e3ebbf1e2dd50dc1a32bf64efe1c5d90e
OVIS <ovi@ovswebsites.com>
2023-12-15 14:44:38 +0200

    updated webflowrc to use env vars only for WF connection

--- .webflowrc.js
+++ .webflowrc.js
@@ -1,9 +1,7 @@
 module.exports = {
   host: "https://api.webflow.com",
   rootDir: "./src/devlink",
-  siteId: "65459ab6440c1238e7ede277", // new site
-  // siteId: "65466717b6d1ea542cbc3459", // old site
-  authToken: process.env.WF_SITE_TOKEN, // NEW site An environment variable is recommended for this field.
-  // authToken: "8e5ccf445612c8fe3c83f8b958fb00a16fa6cf55ed4de3a44b63da1fa2445b02", // OLD site An environment variable is recommended for this field.
+  siteId: process.env.WF_SITE_ID,
+  authToken: process.env.WF_SITE_TOKEN,
   cssModules: false,
 };
